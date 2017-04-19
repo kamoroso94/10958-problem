c10ad6d8a159c9f2df865dfdc969482d028a3022
Kyle Amoroso <kamoroso1994@gmail.com>
2017-04-19 17:07:31 -0400

    Add comments

--- findexpr.c
+++ findexpr.c
@@ -26,69 +26,73 @@ void ExprNode_set(ExprNode* node, char value);
 int concat(int a, int b);
 int isdigit(char c);
 
+// take integer command line argument, output equivalent expression
 int main(int argc, char* argv[]) {
 	int target, success;
-	char expr[128];
-	
+	char expr[128];	// max expression length ~50 chars
+
 	if(argc != 2) {
 		printf("Missing argument\n");
 		return 0;
 	}
-	
+
 	target = atoi(argv[1]);
 	if(target == 0 && argv[1][0] != '0') {
 		printf("Invalid argument\n");
+		return 0;
 	}
-	
+
 	success = findExpr(target, expr);
-	
+
 	printf("Success? %c\n", success ? 'y' : 'n');
 	printf("%i = %s\n", target, expr);
-	
+
 	return 0;
 }
 
+// find an expression for a given target number, copy to result, return success
 int findExpr(int target, char* result) {
 	int success, len;
 	ExprNode* root = ExprNode_create();
-	
+
 	success = _findExpr(target, root, 1);
-	
+
 	if(success) {
 		len = ExprNode_toString(root, result, 0);
 		result[len] = '\0';
 	} else {
 		strcpy(result, "No solution");
 	}
-	
+
 	ExprNode_destroy(root);
-	
+
 	return success;
 }
 
+// brute force search expressions, build tree, return success
 int _findExpr(int target, ExprNode* root, int leafCount) {
 	int i, j, len;
-	char opcodes[OPCODES] = {'|', '+', '-', '*', '/', '^'};
-	ExprNode* leaves[DIGITS];
+	char opcodes[OPCODES] = {'|', '+', '-', '*', '/', '^'};	// check out ExprNode_eval to see the operations these represent
+	ExprNode* leaves[DIGITS];	// grow tree until 9 leaves, list needs no more than 9 elems
 	ExprNode* leaf;
-	
+
 	if(leafCount == DIGITS) {
 		len = ExprNode_leaves(root, leaves, 0);
-		
+
 		for(i = 0; i < len; i++) {
 			leaves[i]->value = '1' + i;
 		}
-		
+
 		return ExprNode_eval(root) == target;
 	}
-	
+
 	for(i = 0; i < OPCODES; i++) {
 		char op = opcodes[i];
 		len = ExprNode_leaves(root, leaves, 0);
-		
+
 		for(j = 0; j < len; j++) {
 			leaf = leaves[j];
-			
+
 			if(leaf->parent != NULL && leaf->parent->value == '|' && op != '|') {
 				continue;
 			}
@@ -101,28 +105,31 @@ int _findExpr(int target, ExprNode* root, int leafCount) {
 		}
 		// try different op
 	}
-	
+
 	return 0;
 }
 
+// make a new expression tree
 ExprNode* ExprNode_create() {
 	ExprNode* root = (ExprNode*) malloc(sizeof(ExprNode));
 	ExprNode_clear(root);
 	root->parent = NULL;
-	
+
 	return root;
 }
 
+// free memory from expression tree
 void ExprNode_destroy(ExprNode* root) {
 	if(root == NULL) {
 		return;
 	}
-	
+
 	ExprNode_destroy(root->left);
 	ExprNode_destroy(root->right);
 	free(root);
 }
 
+// reset the node and delete the branches
 void ExprNode_clear(ExprNode* node) {
 	ExprNode_destroy(node->left);
 	ExprNode_destroy(node->right);
@@ -131,6 +138,7 @@ void ExprNode_clear(ExprNode* node) {
 	node->value = '\0';
 }
 
+// collect the leaves, return length
 int ExprNode_leaves(ExprNode* root, ExprNode** list, int index) {
 	if(ExprNode_isLeaf(root)) {
 		list[index++] = root;
@@ -141,35 +149,45 @@ int ExprNode_leaves(ExprNode* root, ExprNode** list, int index) {
 	return index;
 }
 
+// check if no descendants
 int ExprNode_isLeaf(ExprNode* node) {
 	return node->left == NULL && node->right == NULL;
 }
 
+// evaluate expression tree
 double ExprNode_eval(ExprNode* root) {
 	switch(root->value) {
+		// concatenate
 		case '|':
 			return concat(ExprNode_eval(root->left), ExprNode_eval(root->right));
-		
+
+		// add
 		case '+':
 			return ExprNode_eval(root->left) + ExprNode_eval(root->right);
-		
+
+		// subtract
 		case '-':
 			return ExprNode_eval(root->left) - ExprNode_eval(root->right);
-		
+
+		// multiply
 		case '*':
 			return ExprNode_eval(root->left) * ExprNode_eval(root->right);
-		
+
+		// divide
 		case '/':
 			return ExprNode_eval(root->left) / ExprNode_eval(root->right);
-		
+
+		// exponentiate
 		case '^':
 			return pow(ExprNode_eval(root->left), ExprNode_eval(root->right));
-		
+
+		// digit
 		default:
 			return root->value - '0';
 	}
 }
 
+// put expression tree into string, returns length
 int ExprNode_toString(ExprNode* root, char* expr, int index) {
 	if(root == NULL) {
 		return index;
@@ -188,10 +206,11 @@ int ExprNode_toString(ExprNode* root, char* expr, int index) {
 		index = ExprNode_toString(root->right, expr, index);
 		expr[index++] = ')';
 	}
-	
+
 	return index;
 }
 
+// clear node, set value, add empty descendants
 void ExprNode_set(ExprNode* node, char value) {
 	ExprNode_clear(node);
 	node->value = value;
@@ -201,10 +220,11 @@ void ExprNode_set(ExprNode* node, char value) {
 	node->right->parent = node;
 }
 
+// concatenate two ints
 int concat(int a, int b) {
 	int exp = 1;
 	int n = b;
-	
+
 	if(n == 0) {
 		exp = 10;
 	} else {
@@ -213,10 +233,11 @@ int concat(int a, int b) {
 			n /= 10;
 		}
 	}
-	
+
 	return a * exp + b;
 }
 
+// check if digit char
 int isdigit(char c) {
 	return c >= '0' && c <= '9';
 }
